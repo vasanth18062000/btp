76473581f05c74d53f3ef18ff6d0c5183629e704
Karthikeyan.K <karthikeyan.k@laderatechnology.com>
2023-12-07 19:39:57 +0530

    Hosted&ensured proper running of applications

--- NORTHWIND/srv/catalog-service.cds
+++ NORTHWIND/srv/catalog-service.cds
@@ -2,10 +2,18 @@ using { Northwind as external } from './external/Northwind';
 
 service CatalogService {
     
+    @readonly
     entity Products as projection on external.Products {
          key ID, Name, Description, ReleaseDate, DiscontinuedDate, Rating, Price
     }
+    @readonly
      entity PersonDetail as projection on external.PersonDetails {
          key PersonID,Age,Gender,Phone
     }
+
+    
 }
+
+annotate CatalogService.Products with @cds.persistence:{table,skip:false};
+
+annotate CatalogService.PersonDetail with @cds.persistence:{table,skip:false};
--- NORTHWIND/srv/catalog-service.js
+++ NORTHWIND/srv/catalog-service.js
@@ -1,17 +1,24 @@
 const cds = require('@sap/cds');
 
-module.exports = cds.service.impl(async function() {
-	const { Products } = this.entities;
-	const service = await cds.connect.to('Northwind');
-	this.on('READ', Products, request => {
-		return service.tx(request).run(request.query);
-	});
-});
+// module.exports = cds.service.impl(async function() {
+// 	const { Products } = this.entities;
+// 	const service = await cds.connect.to('Northwind');
+// 	this.on('READ', Products, request => {
+// 		return service.tx(request).run(request.query);
+// 	});
+// });
 
 module.exports = cds.service.impl(async function() {
 	const { PersonDetail } = this.entities;
 	const service = await cds.connect.to('Northwind');
 	this.on('READ', PersonDetail, request => {
 		return service.tx(request).run(request.query);
+	}),
+
+	  { Products } = this.entities;
+	const service1 = await cds.connect.to('Northwind');
+	this.on('READ', Products, request => {
+		return service.tx(request).run(request.query);
 	});
+
 });
