9eca60bf23bf403f1f52ec8a5434551972c8ba48
Elumalai M <elumalai.m@laderatechnology.com>
2023-12-08 06:44:24 +0000

    part 13 changes

--- bookshop/app/browse/webapp/custom/AddReview/AddReviewButtonHandler.js
+++ bookshop/app/browse/webapp/custom/AddReview/AddReviewButtonHandler.js
@@ -1,9 +1,43 @@
-sap.ui.define(["sap/m/MessageBox"], function (MessageBox) {
+sap.ui.define(
+  ["sap/ui/core/Fragment", "./AddReviewDialogHandler"],
+  function (Fragment, AddReviewDialogHandler) {
     "use strict";
 
-    return{
-        onPress: function () {
-            MessageBox.show("Button pressed!");
-        },
+    return {
+      openDialog: async function (oEvent) {
+        const sRowBindingPath = oEvent
+          .getSource()
+          .getParent()
+          .getParent()
+          .getBindingContextPath();
+
+        const oBooklistPage = sap.ui.getCore().byId("bookshop::BooksList");
+
+        if (!this.oAddReviewDialog) {
+          this.sReviewDialogId = `${oBooklistPage.getId()}-AddReviewDialog`;
+          this.oAddReviewDialog = await Fragment.load({
+            id: this.sReviewDialogId,
+            name: "bookshop.custom.AddReview.AddReviewDialog",
+          });
+          oBooklistPage.addDependent(this.oAddReviewDialog);
+        }
+
+        const oParams = {
+          sRowBindingPath,
+          sReviewDialogId: this.sReviewDialogId,
+        };
+
+        this.oAddReviewDialog.attachBeforeOpen(
+          oParams,
+          AddReviewDialogHandler.beforeOpenDialog
+        );
+
+        this.oAddReviewDialog.open();
+
+        this.oAddReviewDialog.detachBeforeOpen(
+          AddReviewDialogHandler.beforeOpenDialog
+        );
+      },
     };
-});
+  }
+);
---
+++ bookshop/app/browse/webapp/custom/AddReview/AddReviewDialogHandler.js
@@ -0,0 +1,81 @@
+sap.ui.define(
+    [
+      "sap/ui/core/Fragment",
+      "./createAddReviewFormContainer",
+      "sap/ui/model/json/JSONModel",
+    ],
+    function (Fragment, createAddReviewFormContainer, JSONModel) {
+      "use strict";
+  
+      const getAddReviewDialog = (oEvent) => oEvent.getSource().getParent();
+  
+      const setInputError = (oInputElement, bHasError) => {
+        const oFormErrorModel = oInputElement.getModel("formErrors");
+        const oInputErrors = { ...oFormErrorModel.getProperty("/inputErrors") };
+        oInputErrors[oInputElement.getId()] = bHasError;
+        oFormErrorModel.setProperty("/inputErrors", oInputErrors);
+      };
+  
+      return {
+        beforeOpenDialog: function (oEvent, oParams) {
+          const oFormErrorModel = new JSONModel({
+            get hasErrors() {
+              return Object.values(this.inputErrors).some((error) => error);
+            },
+            inputErrors: {},
+          });
+          oEvent.getSource().setModel(oFormErrorModel, "formErrors");
+  
+          const { sRowBindingPath, sReviewDialogId } = oParams;
+          this.sReviewDialogId = sReviewDialogId;
+          const oAddReviewForm = Fragment.byId(sReviewDialogId, "addReviewForm");
+  
+          oAddReviewForm.bindAggregation("formContainers", {
+            path: `${sRowBindingPath}/reviews`,
+            template: createAddReviewFormContainer(),
+            length: 1,
+            parameters: {
+              $$updateGroupId: "reviews",
+            },
+          });
+  
+          const oReviewBinding = oAddReviewForm.getBinding("formContainers");
+  
+          oReviewBinding.create({
+            rating: 0,
+            title: "",
+            text: "",
+          });
+        },
+  
+        submit: async function (oEvent) {
+          const oAddReviewDialog = getAddReviewDialog(oEvent);
+          oAddReviewDialog.setBusy(true);
+  
+          try {
+            await oAddReviewDialog.getModel().submitBatch("reviews");
+            console.log("SUCCESS");
+            oAddReviewDialog.close();
+          } catch (error) {
+            console.log(`ERROR: ${error.message}`);
+          } finally {
+            oAddReviewDialog.setBusy(false);
+          }
+        },
+  
+        cancel: function (oEvent) {
+          getAddReviewDialog(oEvent).close();
+        },
+  
+        onValidationError: function (oEvent) {
+          const oInputElement = oEvent.getParameter("element");
+          setInputError(oInputElement, true);
+        },
+  
+        onValidationSuccess: function (oEvent) {
+          const oInputElement = oEvent.getParameter("element");
+          setInputError(oInputElement, false);
+        },
+      };
+    }
+  );
