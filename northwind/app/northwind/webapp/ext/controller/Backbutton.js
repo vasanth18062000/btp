95348b88b193c0f265775b8068e28a0bc0a5e83e
Vasanth Karthikeyan <vasanth.karthikeyan@laderatechnology.com>
2023-12-13 18:56:46 +0530

    northwind commit

---
+++ northwind/app/northwind/webapp/ext/controller/Backbutton.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/m/MessageToast"
+], function(MessageToast) {
+    'use strict';
+
+    return {
+        backbutton: function(oEvent) {
+            window.location="http://localhost:4004/northwind/webapp/index.html";        }
+    };
+});
