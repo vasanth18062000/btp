67af577b3b883a59ed8267d17231ed76878bd2c1
Vasanth Karthikeyan <vasanth.karthikeyan@laderatechnology.com>
2024-01-08 18:31:58 +0530

    ui5 car commit

--- ui5/carmanagement/webapp/index.js
+++ ui5/carmanagement/webapp/index.js
@@ -1,6 +1,8 @@
-sap.ui.define([], () => {
+sap.ui.define([
+	"sap/ui/core/mvc/XMLView"
+], (XMLView) => {
 	"use strict";
-	alert("UI5 is ready");
+	XMLView.create({
+		viewName: "carmanagement.view.App"
+	}).then((oView) => oView.placeAt("content"));
 });
-               
-   
