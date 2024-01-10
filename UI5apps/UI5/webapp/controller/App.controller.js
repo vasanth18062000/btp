931f14934bc1c65dc2f8bd9a47b1f61943293309
Gobilkannan.S <gobikannan.sundaram@laderatechnology.com>
2024-01-10 18:52:20 +0530

    updated

--- UI5apps/UI5/webapp/controller/App.controller.js
+++ UI5apps/UI5/webapp/controller/App.controller.js
@@ -1,45 +1,36 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+], function(Controller,MessageToast) {
+   'use strict';
+   return Controller.extend("ui5.controller.App",{
+       
+           // onInit(){
+           //     // set data model on view
+           //     const oData={
+           //         recipient:{
+           //         name:"india"
+           //     }
+           // };
+           // const oModel =new JSONModel(oData);
+           // this.getView().setModel(oModel);
 
- ], (Controller,MessageToast,JSONModel,ResourceModel) => {
-    "use strict";
- 
-    return Controller.extend("ui5.controller.App", {
+           // var i18nModel=new ResourceModel({
+           //     bundleName:"carmanagement.i18n.i18n"
+           // });
+           // this.getView().setModel(i18nModel,"i18n");
+           // },
+           onShowHello:function(){
+               alert("hey gopi click here.. to OK");
+               MessageToast.show("Hiii Gopikannan...");
+                 // read msg from i18n model
+       //  const oBundle = this.getView().getModel("i18n").getResourceBundle();
+       //  const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+       //  const sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
-
-      onInit : function(){ 
-
-
-         var oData ={
-            recipient :{
-               name: "computer"
+        // show message
+       //  MessageToast.show(sMsg);
             }
-         };
-
-         var oModel =new JSONModel(oData);
-         this.getView().setModel(oModel);
-
-         //set Model on view 
-
-         var i18nModel =new ResourceModel({
-             bundleName: "ui5.i18n.i18n"
-         });
-
-         this.getView().setModel(i18nModel,"i18n"); 
-
-
-      },
-
-         onShowHello :function() { 
-          // -------*show a native JavaScript alert*----------------
-
-
-         // alert("Hello World");
-         MessageToast.show("Hello Gopikannan Welcome!!!");
-
-       }
-    });
- });
+       
+   });
+});
