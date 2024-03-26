9f3bb10e0477ff6cfff22c2e75117f54bfe0cb99
Vasanth Karthikeyan <vasanth.karthikeyan@laderatechnology.com>
2024-03-26 10:35:33 +0530

    26/03 first

--- northwindext/srv/catalog-service.js
+++ northwindext/srv/catalog-service.js
@@ -4,9 +4,9 @@ module.exports = cds.service.impl(async function() {
 	const { Products } = this.entities;
 	const service = await cds.connect.to('NorthWind');
 	this.on('READ', Products, request => {
-		const LOG=cds.log('connection logger');
-		LOG.info('connection logger entered');
-		LOG.info('connection logger ended');	
+		// const LOG=cds.log('connection logger');
+		// LOG.info('connection logger entered');
+		// LOG.info('connection logger ended');	
 		return service.tx(request).run(request.query);
 	});
 });
